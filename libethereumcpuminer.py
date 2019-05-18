a51bebcae0256405eda1731c0fcd1eae19395930
Michael Seiler <michaelseiler@Michaels-MacBook-Pro.local>
2019-05-18 08:49:56 -0400

    re-send the mining hash as the mix digest because why not

--- libethereumcpuminer.py
+++ libethereumcpuminer.py
@@ -38,12 +38,12 @@ class EthereumCpuMiner(object):
         nonce = start_nonce
         i = 0
         difficulty = int(bin_to_hex_b(self._target_bin)[2:],16)
-        print(difficulty)
+        print('difficulty',difficulty)
         cnt = 0
         while True:
             i += 1
             cnt+=1
-            bin_nonce = utils.zpad(utils.int_to_big_endian((nonce + i) & TT64M1), 8)
+            bin_nonce = utils.zpad(utils.int_to_big_endian((nonce + i)  ), 8)
             k = sha3.keccak_256()
             k.update(self._mining_hash_bin)
             k.update(bin_nonce)
@@ -51,12 +51,19 @@ class EthereumCpuMiner(object):
             #o = hashimoto_light(self._block_number_int, cache, self._mining_hash_bin, bin_nonce)
             if hash1 <= difficulty:
                 self._nonce_bin = bin_nonce
-                print(cnt)
+                print('cnt',cnt)
+                for x in self._mining_hash_bin:
+                  print(x)
+                for x in bin_nonce:
+                  print(x)
+                print('mh', self._mining_hash_bin, len( self._mining_hash_bin))
+                print('nonce', bin_nonce, len( bin_nonce))
                 return
 
     def submit_work(self):
         print("self nonce is", self._nonce_bin)
         nonce_hex, mix_digest_hex = bin_to_hex_b_final(self._nonce_bin), "0x"+"0"*64
+        mix_digest_hex = self._mining_hash_hex
         print([nonce_hex, self._mining_hash_hex, mix_digest_hex])
         self._conn.manager.request_blocking("eth_submitWork", [nonce_hex, self._mining_hash_hex, mix_digest_hex])
 
