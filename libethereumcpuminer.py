c7323a3a63add409873406f4ca605fc410424b03
Your Name <you@example.com>
2019-10-26 18:21:58 +0000

    Verificaiton strings

--- libethereumcpuminer.py
+++ libethereumcpuminer.py
@@ -28,9 +28,17 @@ class EthereumCpuMiner(object):
         self._nonce_bin = None
         self._mix_digest_bin = None
 
+        print(
+        """                                                       
+         _____ _____ _____ ___    _____            _____ _             
+        |   __|  |  |  _  |_  |  |_   _|___ _ _   |     |_|___ ___ ___ 
+        |__   |     |     |_  |    | | | . | | |  | | | | |   | -_|  _|
+        |_____|__|__|__|__|___|    |_| |___|_  |  |_|_|_|_|_|_|___|_|  
+        				   |___|                        
+        """)
         k = sha3.keccak_256()
         k.update('ETC'.encode('utf-8'))
-        print('CONTROL HASH ETC: '+k.hexdigest())
+        print('KECCAK256 CONTROL HASH OF "ETC": '+k.hexdigest() + '\n')
 
     def get_work(self):
         self._mining_hash_hex, _, target_hex, block_number_hex = self._conn.eth.getWork()
@@ -42,8 +50,10 @@ class EthereumCpuMiner(object):
         nonce = start_nonce
         i = 0
         difficulty = int(bin_to_hex_b(self._target_bin)[2:],16)
-        print('difficulty',difficulty)
+        print('Difficulty: ',difficulty)
+        print('Block Number: ', self._block_number_int)
         cnt = 0
+        start = time.time()
         while True:
             i += 1
             cnt+=1
@@ -54,49 +64,56 @@ class EthereumCpuMiner(object):
             hash1 = int(k.hexdigest(), 16)
             # o = hashimoto_light(self._block_number_int, cache, self._mining_hash_bin, bin_nonce)
             if hash1 <= difficulty:
+                end = time.time()
                 self._nonce_bin = bin_nonce
-                print('cnt',cnt)
+                print('Mining Cycles: ',cnt)
                 # for x in self._mining_hash_bin:
                 #   print(x)
                 # for x in bin_nonce:
                 #   print(x)
-                print('mh', self._mining_hash_bin, len( self._mining_hash_bin))
-                print('nonce', bin_nonce, len( bin_nonce))
+                # print('Nonce: ', bin_nonce, len( bin_nonce))
+                print('Block Mining Time: ', end - start)
                 return
 
     def submit_work(self):
-        print("self nonce is: ", self._nonce_bin)
-        nonce_hex, mix_digest_hex = bin_to_hex_b_final(self._nonce_bin), "0x"+"0"*64
-        mix_digest_hex = self._mining_hash_hex
+        nonce_hex, mix_digest_hex = bin_to_hex_b_final(self._nonce_bin), "0x"+"1"*64
+        # mix_digest_hex = self._mining_hash_hex
 
         work_list = [nonce_hex, self._mining_hash_hex, mix_digest_hex]
-        print('work list is: ', work_list)
 
         nonce_hasher = sha3.keccak_256()
        	nonce_utf8 = str(self._nonce_bin)
         nonce_hasher.update(nonce_utf8.encode('utf-8'))
         # nonce_hasher.update(self._nonce_bin.encode('utf-8'))
-        print('NONCE HASH: ' + nonce_hasher.hexdigest())
+        print("    NONCE IS: ", self._nonce_bin)
 
         mix_hasher = sha3.keccak_256()
-        mix_hasher.update(("0x"+"0"*64).encode('utf-8'))
-        print('MIX HASH: ' + mix_hasher.hexdigest())
+        mix_hasher.update((mix_digest_hex).encode('utf-8'))
+
+        mining_hash_hasher = sha3.keccak_256()
+        mining_hash_hasher.update(self._mining_hash_hex.encode('utf-8'))
+        print("    MINING HASH IS: ", self._mining_hash_hex)
 
         work_hasher = sha3.keccak_256()
         work_concat = ''.join(work_list)
-        print(work_concat)
         work_hasher.update(work_concat.encode('utf-8'))
-        print('WORK HASH: ' + work_hasher.hexdigest())
+        print('    WORK LIST: ', work_list)
+
+        print('\n')
+        print('    VERIFICATION HASH OF NONCE: ', nonce_hasher.hexdigest())
+        print('    VERIIFCATION HASH OF MINING HASH: ', mining_hash_hasher.hexdigest())
+        print('    VERIFICATION HASH OF MIX: ', mix_hasher.hexdigest())
+        print('    VERIFICATION HASH OF WORK LIST: ', work_hasher.hexdigest())
 
         self._conn.manager.request_blocking("eth_submitWork", work_list)
 
     def mine_n_blocks(self, n=1):
         for _ in range(n):
             self.get_work()
-            print("got work")
+            print("**Got Work**")
             self.mine()
-            print('done mine')
+            print('**Done Mining Block**')
             self.submit_work()
-            print('submit answer')
+            print('**Submit Answer**')
             # time.sleep(10)
 
